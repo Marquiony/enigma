dc017a67dcdbe40fd59fac5a0bdfd9c6db6ea254
Marquiony <83370970+Marquiony@users.noreply.github.com>
2024-06-06 16:49:06 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,98 @@
+import streamlit as st
+
+st.write(""" 
+# Bem vindo a criptografia da máquina enigma
+""")
+
+class Rotor:
+    def __init__(self, wiring, notch):
+        self.wiring = wiring
+        self.notch = notch
+        self.position = 0
+
+    def set_position(self, pos):
+        self.position = (ord(pos.upper()) - ord('A')) % 26
+
+    def rotate(self):
+        self.position = (self.position + 1) % 26
+        return self.position == self.notch
+
+    def encrypt_forward(self, c):
+        idx = (ord(c) - ord('A') + self.position) % 26
+        encrypted_char = self.wiring[idx]
+        return chr((ord(encrypted_char) - ord('A') - self.position + 26) % 26 + ord('A'))
+
+    def encrypt_backward(self, c):
+        idx = (ord(c) - ord('A') + self.position) % 26
+        encrypted_char = chr((self.wiring.index(chr(idx + ord('A'))) - self.position + 26) % 26 + ord('A'))
+        return encrypted_char
+
+class Reflector:
+    def __init__(self, wiring):
+        self.wiring = wiring
+
+    def reflect(self, c):
+        return self.wiring[ord(c) - ord('A')]
+
+class Plugboard:
+    def __init__(self, wiring):
+        self.wiring = wiring
+
+    def swap(self, c):
+        return self.wiring.get(c, c)
+
+class Enigma:
+    def __init__(self, rotors, reflector, plugboard):
+        self.rotors = rotors
+        self.reflector = reflector
+        self.plugboard = plugboard
+
+    def set_rotor_positions(self, positions):
+        for rotor, pos in zip(self.rotors, positions):
+            rotor.set_position(pos)
+
+    def encrypt(self, message):
+        encrypted_message = ''
+        for c in message:
+            if c.isalpha():
+                c = c.upper()
+                c = self.plugboard.swap(c)
+                for rotor in self.rotors:
+                    c = rotor.encrypt_forward(c)
+                c = self.reflector.reflect(c)
+                for rotor in reversed(self.rotors):
+                    c = rotor.encrypt_backward(c)
+                c = self.plugboard.swap(c)
+                encrypted_message += c
+                for rotor in self.rotors:
+                    if not rotor.rotate():
+                        break
+            else:
+                encrypted_message += c
+        return encrypted_message
+
+# Configuração dos rotores
+rotor1 = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 17)  # Rotor I
+rotor2 = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", 5)   # Rotor II
+rotor3 = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", 22)  # Rotor III
+reflector = Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")  # Reflector B
+plugboard = Plugboard({'A': 'B', 'B': 'A', 'C': 'D', 'D': 'C'})  # Exemplo de plugboard
+
+enigma = Enigma([rotor1, rotor2, rotor3], reflector, plugboard)
+
+st.header("Escolha as configurações da máquina")
+
+# Solicita a configuração inicial dos rotores
+initial_positions = st.text_input("Digite a posição inicial dos rotores (por exemplo, ABC): ").upper()
+
+# Entrada da mensagem a ser criptografada
+message = st.text_area("Digite a mensagem para criptografar:")
+
+# Botão para criptografar
+if st.button("Criptografar"):
+    if initial_positions and message:
+        enigma.set_rotor_positions(initial_positions)
+        encrypted_message = enigma.encrypt(message)
+        st.write(f"Mensagem Criptografada: {encrypted_message}")
+    else:
+        st.write("Por favor, insira a posição inicial dos rotores e a mensagem.")
